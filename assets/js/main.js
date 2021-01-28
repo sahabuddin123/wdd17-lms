e11ecc88144e83670b666f7bac31f64db4b34cff
sahabuddin <sahabuddinriyaj984@gmail.com>
2021-01-28 11:34:11 +0600

    testimonial complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+$('.testimonial').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -15,4 +36,4 @@ $('.owl-carousel').owlCarousel({
             items:3
         }
     }
-})
+});
